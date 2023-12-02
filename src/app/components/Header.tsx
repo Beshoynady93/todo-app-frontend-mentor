7cfe57878bad6b0aa474c1e036f0507e23528440
Beshoynady93 <101524188+Beshoynady93@users.noreply.github.com>
2023-12-02 19:40:17 +0200

    fixing deployment issues

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,23 +1,26 @@
-"use client";
+'use client';
 
-import React from "react";
-import SwitchThemeBtn from "./SwitchThemeBtn";
-import { useThemeContext } from "../contexts/ThemeContext";
+import React, { useEffect, useState } from 'react';
+import SwitchThemeBtn from './SwitchThemeBtn';
+import { useThemeContext } from '../contexts/ThemeContext';
 
 const Header = () => {
   const { theme } = useThemeContext();
 
-  const BG =
-    theme === "light" && window.innerWidth < 375
-      ? "bg-header-mobile-light"
-      : theme === "dark" && window.innerWidth < 375
-        ? "bg-header-mobile-dark"
-        : theme === "light" && window.innerWidth > 375
-          ? "bg-header-desktop-light"
-          : "bg-header-desktop-dark";
+  const [background, setBackground] = useState('');
+
+  useEffect(() => {
+    theme === 'light' && window.innerWidth < 375
+      ? setBackground('bg-header-mobile-light')
+      : theme === 'dark' && window.innerWidth < 375
+        ? setBackground('bg-header-mobile-dark')
+        : theme === 'light' && window.innerWidth > 375
+          ? setBackground('bg-header-desktop-light')
+          : setBackground('bg-header-desktop-dark');
+  }, [theme]);
 
   return (
-    <header className={`${BG} min-h-[10rem] px-4 py-8`}>
+    <header className={`${background} min-h-[10rem] px-4 py-8`}>
       <div className="mx-auto flex max-w-lg items-center justify-between">
         <h1 className="text-2xl font-extrabold tracking-[0.5rem] text-white">
           TODO
